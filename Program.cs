21e9d2145ddbae99be39023a60e80fc1dbff4064
skid <skid@kinkpad.local>
2025-12-13 21:54:53 +0100

    added function parseBinOp

--- Program.cs
+++ Program.cs
@@ -4,39 +4,26 @@
     double value;
     bool isLiteral = Double.TryParse(s, out value);
     if (isLiteral)       return new Literal(value);
-    if (s.Contains('+')) {
-        string[] split = s.Split('+', 2); // divido la stringa in due dove trovo il simbolo
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.SUM, l, r);
-    }
-    if (s.Contains('-')) {
-        string[] split = s.Split('-', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.SUB, l, r);
-    }
-    if (s.Contains('*')) {
-        string[] split = s.Split('*', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.MUL, l, r);
-    }
-    if (s.Contains('/')) {
-        string[] split = s.Split('/', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.DIV, l, r);
-    }
+    if (s.Contains('+')) return parseBinOp('+', BinOp.Kind.SUM, s);
+    if (s.Contains('-')) return parseBinOp('-', BinOp.Kind.SUB, s);
+    if (s.Contains('*')) return parseBinOp('*', BinOp.Kind.MUL, s);
+    if (s.Contains('/')) return parseBinOp('/', BinOp.Kind.DIV, s);
 
     throw new Exception("Espressione non calcolabile");
+}
+
+BinOp parseBinOp(char symbol, BinOp.Kind kind, string s) {
+    string[] split = s.Split(symbol, 2);
+    Expr l = parseExpr(split[0]);
+    Expr r = parseExpr(split[1]);
+    return new BinOp(kind, l, r);
 }
 
 string s = "12 / 3.7 + 2 * 0.5";
 
 try {
-    Expr result = parseExpr(s);
-    Console.WriteLine(s + " = " + result.eval());
+    Expr expr = parseExpr(s);
+    Console.WriteLine(s + " = " + expr.eval());
 } catch (Exception e) {
-    Console.WriteLine("Espressione non valida");
+    Console.WriteLine(e.ToString());
 }
