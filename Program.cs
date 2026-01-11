3d9e675c99b49d54976518949714e74cf91a05db
skid <skid@kinkpad.local>
2026-01-11 14:32:08 +0100

    added parseBinOp function

--- Program.cs
+++ Program.cs
@@ -6,34 +6,22 @@ Expr parseExpr(string s) {
     bool isLiteral = Double.TryParse(s, out value);
     if (isLiteral) return new Literal(value);
 
-    if (s.Contains('+')) {
-        string[] split = s.Split('+', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.SUM, l, r);
-    }
-    if (s.Contains('-')) {
-        string[] split = s.Split('-', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.SUB, l, r);
-    }
-    if (s.Contains('*')) {
-        string[] split = s.Split('*', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.MUL, l, r);
-    }
-    if (s.Contains('/')) {
-        string[] split = s.Split('/', 2);
-        Expr l = parseExpr(split[0]);
-        Expr r = parseExpr(split[1]);
-        return new BinOp(BinOp.Kind.DIV, l, r);
-    }
+    if (s.Contains('+')) return parseBinOp('+', BinOp.KIND.SUM, s);
+    if (s.Contains('-')) return parseBinOp('-', BinOp.KIND.SUB, s);
+    if (s.Contains('*')) return parseBinOp('*', BinOp.KIND.MUL, s);
+    if (s.Contains('/')) return parseBinOp('/', BinOp.KIND.DIV, s);
 
     throw new Exception("Expression can't be parsed");
 }
 
+BinOp parseBinOp(char symbol, BinOp.KIND kind, string s) {
+    string[] split = s.Split(symbol, 2);
+    Expr l = parseExpr(split[0]);
+    Expr r = parseExpr(split[1]);
+    return new BinOp(kind, l, r);
+}
+
+// REPL
 string? input = Console.ReadLine();
 while (input != null && !input.Equals("bye")) {
     try {
